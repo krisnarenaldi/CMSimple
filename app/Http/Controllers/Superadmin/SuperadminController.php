fbf5ff8e6504a0e29f51d87825b068169bf6f99c
Krisna Renaldi <krisna.renaldi@gmail.com>
2019-05-26 09:35:01 +0000

    test change

--- app/Http/Controllers/Superadmin/SuperadminController.php
+++ app/Http/Controllers/Superadmin/SuperadminController.php
@@ -5,25 +5,12 @@ namespace App\Http\Controllers\Superadmin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Support\Facades\Auth;
-
 class SuperadminController extends Controller
 {
     //
     
-    use AuthenticatesUsers;
-
-    protected $guard = 'admin';
-    protected $redirectTo = '/admin';
-
-
-    public function __construct(){
-        $this->middleware('guest:admin,admin')->except('logout');
-    }
-    
     public function index(){
-        return view("admin.login");
+        echo "I am here";
     }
     
     public function hello($name = "you"){
